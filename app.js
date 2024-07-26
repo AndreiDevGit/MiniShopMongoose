6c65e79e55b990ff74ade21b2f42ae86ec04041b
AndreiDev <a.v.babici@gmail.com>
2024-07-26 06:59:36 +0300

    Adding and Using a User Model

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 const errorController = require('./controllers/error')
-// const User = require('./models/user')
+const User = require('./models/user')
 
 const app = express()
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-// app.use((req, res, next) => {
-//   User.findById('66a034ea24b128f625380c2d')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id)
-//       next()
-//     })
-//     .catch(err => console.log(err))
-// })
+app.use((req, res, next) => {
+  User.findById('66a31deade650b15d3c0e362')
+    .then(user => {
+      req.user = user
+      next()
+    })
+    .catch(err => console.log(err))
+})
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
@@ -34,6 +34,19 @@ app.use(errorController.get404)
 
 mongoose.connect('mongodb+srv://andreidev:andreidev@project0.ee9xrg1.mongodb.net/shop?retryWrites=true&w=majority&appName=Project0')
   .then(result => {
+      User.findOne().then(user => {
+        if (!user) {
+          const user = new User({
+            name: 'Andrei',
+            email: 'test@mail.com',
+            cart: {
+              items: []
+            }
+          })
+          user.save()
+        }
+      })
+
       app.listen(3000)
     }
   ).catch(err => console.log(err))
