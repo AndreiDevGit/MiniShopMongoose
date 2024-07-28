d88dbf942fdfb7c63b33418456e62b04cd67c777
AndreiDev <a.v.babici@gmail.com>
2024-07-28 04:45:58 +0300

    using sesstion to login

--- app.js
+++ app.js
@@ -36,15 +36,6 @@ app.use(
   })
 )
 
-app.use((req, res, next) => {
-  User.findById('66a31deade650b15d3c0e362')
-    .then(user => {
-      req.user = user
-      next()
-    })
-    .catch(err => console.log(err))
-})
-
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 app.use(authRoutes)
