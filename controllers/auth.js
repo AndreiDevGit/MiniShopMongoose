802194db8d1c0670d0730079ae56aae15abbfed5
AndreiDev <a.v.babici@gmail.com>
2024-08-02 10:26:38 +0300

    implementing get token logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto')
+
 const bcrypt = require('bcryptjs')
 const nodemailer = require('nodemailer')
 const sendgridTransport = require('nodemailer-sendgrid-transport')
@@ -76,7 +78,10 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
-        req.flash('error', 'E-mail exists already, please pick a different one!')
+        req.flash(
+          'error',
+          'E-Mail exists already, please pick a different one.'
+        )
         return res.redirect('/signup')
       }
       return bcrypt
@@ -93,11 +98,13 @@ exports.postSignup = (req, res, next) => {
           res.redirect('/login')
           return transporter.sendMail({
             to: email,
-            from: 'Andrei@Developer.md',
-            subject: 'Signup Succeeded!',
-            html: '<h1>You successfully singed up! </h1>'
-          }).catch(err => console.log(err))
-
+            from: 'shop@node-complete.com',
+            subject: 'Signup succeeded!',
+            html: '<h1>You successfully signed up!</h1>'
+          })
+        })
+        .catch(err => {
+          console.log(err)
         })
     })
     .catch(err => {
@@ -123,5 +130,40 @@ exports.getReset = (req, res, next) => {
     path: '/reset',
     pageTitle: 'Reset Password',
     errorMessage: message
+  })
+}
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err)
+      return res.redirect('/reset')
+    }
+    const token = buffer.toString('hex')
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          req.flash('error', 'No account with that email found.')
+          return res.redirect('/reset')
+        }
+        user.resetToken = token
+        user.resetTokenExpiration = Date.now() + 3600000
+        return user.save()
+      })
+      .then(result => {
+        res.redirect('/')
+        transporter.sendMail({
+          to: req.body.email,
+          from: 'shop@node-complete.com',
+          subject: 'Password reset',
+          html: `
+            <p>You requested a password reset</p>
+            <p>Click this <a href='http://localhost:3000/reset/${token}'>link</a> to set a new password.</p>
+          `
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
   })
 }
