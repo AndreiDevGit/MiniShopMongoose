50232485af9c75588cf8c7759e784f350f64651b
AndreiDev <a.v.babici@gmail.com>
2024-08-25 14:16:01 +0300

    add async delete product functionality/refactoring postDelet

--- controllers/admin.js
+++ controllers/admin.js
@@ -162,8 +162,25 @@ exports.getProducts = (req, res, next) => {
     .catch(err => console.log(err))
 }
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId
+// exports.postDeleteProduct = (req, res, next) => {
+//   const prodId = req.body.productId
+//   Product.findById(prodId)
+//     .then(product => {
+//       if (!product) {
+//         return next(new Error('Product not found.'))
+//       }
+//       fileHelper.deleteFile(product.imageUrl)
+//       return Product.deleteOne({ _id: prodId, userId: req.user._id })
+//     })
+//     .then(() => {
+//       console.log('DESTROYED PRODUCT')
+//       res.redirect('/admin/products')
+//     })
+//     .catch(err => console.log(err))
+// }
+
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId
   Product.findById(prodId)
     .then(product => {
       if (!product) {
@@ -174,7 +191,9 @@ exports.postDeleteProduct = (req, res, next) => {
     })
     .then(() => {
       console.log('DESTROYED PRODUCT')
-      res.redirect('/admin/products')
+      res.status(200).json({ message: 'Success!' })
     })
-    .catch(err => console.log(err))
+    .catch(err => {
+      res.status(500).json({ message: 'Deleting product failed.' })
+    })
 }
